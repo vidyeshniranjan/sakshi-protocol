c363b0d981c1c9355beabb91c66a242a33327198
vidyeshniranjan <vidyesh.niranjan@gmail.com>
2026-04-27 18:18:22 +0530

    Update controller.py
    Updated to include grounding

--- Version 2.0/Implementation/core/controller.py
+++ Version 2.0/Implementation/core/controller.py
@@ -1,9 +1,10 @@
 def decide(state, distortion):
-    if distortion < 0.3:
+
+    if distortion < 0.25:
         return "accept"
+
     elif distortion < 0.4:
-        return "retrieve"   
-    elif distortion < 0.6:
-        return "revise"
+        return "retrieve"   # Ω activation zone
+
     else:
         return "abstain"
