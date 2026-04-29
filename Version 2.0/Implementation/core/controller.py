d257f546ca6a6edbd10b58ec60c1a8eb5e88e322
vidyeshniranjan <vidyesh.niranjan@gmail.com>
2026-04-29 15:15:29 +0530

    Update controller.py
    Thresholds adjusted

--- Version 2.0/Implementation/core/controller.py
+++ Version 2.0/Implementation/core/controller.py
@@ -1,9 +1,9 @@
 def decide(state, distortion):
 
-    if distortion < 0.20:
+    if distortion < 0.25:
         return "accept"
 
-    elif distortion < 0.30:
+    elif distortion < 0.35:
         return "retrieve"   # Ω activation zone
 
     else:
